63e9cc952288e06b6464e53a8a8c67b65ae3412e
alexish5832 <144606285+alexish5832@users.noreply.github.com>
2023-10-14 23:53:52 +0300

    Add files via upload

---
+++ mem.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import os, random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def programist(ctx):
+    img_name = random.choice(os.listdir('images_programist'))
+    with open(f'images_programist/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+bot.run("MTE1ODAxOTQ2MTM3MTEzNDA5Mw.G2xbnB.7Z4uJYnQUs3o6ZLF9AEsjxm_jxrVFLdPyaKuBM")
